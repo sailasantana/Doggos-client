dd9493c6a373c5b222691b58517cf68f6a68b3c5
sailasantana94 <sk4862@nyu.edu>
2021-01-07 14:11:38 -0500

    hide secrets

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <div style={{ width: "50vw", height: "75vh" }} className = "map"> 
          <MapWrapped
                 googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-                  "AIzaSyAZ9e8yrmg_qJFoBB7Giz4ZKzQNPl7fDm4"
+                  process.env.key
                    }`}
                    loadingElement={<div style={{ height: "100%" }} />}
                       containerElement={<div style={{ height: "100%" }} />}
